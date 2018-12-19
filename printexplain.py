8a7d2cd092a72e1ddd22d4063471496d04148741
shivapriyat <shivapriya.t@gmail.com>
2018-12-20 01:42:26 +0530

    Delete printexplain.py

--- printexplain.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# format -method of string object
-#{} is place holder to replae wih value
-x=42
-print('hello world x is {}'.format(x))
-print('Hello, world %d'%x)
-print(f'hello,world. {x}')
-
