d6a970dfd9508b72d979d4f05021f3b4d106eee9
shivapriyat <shivapriya.t@gmail.com>
2018-12-20 01:40:29 +0530

    Delete MainAndUserMethod.py

--- MainAndUserMethod.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import platform
-
-def main():
-    message()
-def message():
-    print('the python version = {}'.format(platform.python_version()))
-if __name__=='__main__':main()
